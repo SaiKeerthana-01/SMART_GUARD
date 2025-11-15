120bede199f08fe6cead9a9140fbdc9e154dcafd
Keerthana <23110229@iitgn.ac.in>
2025-11-15 08:39:44 +0530

    Delete fix_hosts.py

--- fix_hosts.py
+++
@@ -1,6 +0,0 @@
-for h in net.hosts:
-    h.cmd("echo nameserver 8.8.8.8 > /etc/resolv.conf")
-    h.cmd("ip route add default via 10.0.0.254")
-    h.cmd("apt-get update")
-    h.cmd("apt-get install -y python3 python3-pip")
-    h.cmd("pip3 install paho-mqtt")
